6eb18be267707121fc042189db45697d63fc8ef2
codephillip <codephillip@gmail.com>
2021-07-16 10:44:37 +0300

    added order_tracking_number to order serializer

--- app/serializers.py
+++ app/serializers.py
@@ -152,7 +152,7 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id', 'created_at', 'status', 'valid', 'delivery_method', 'expected_delivery_date_time',
                   'delivery_date_time', 'total_amount', 'payments', 'driver', 'location', 'delivery_speed',
-                  'orderitems', 'customer', 'delivery_fee', 'sub_total_amount', 'created_at']
+                  'orderitems', 'customer', 'delivery_fee', 'sub_total_amount', 'order_tracking_number', 'created_at']
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
