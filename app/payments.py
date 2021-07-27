223a98b11b52c942e80300bbafd2d63fb467d159
codephillip <codephillip@gmail.com>
2021-07-27 22:50:01 +0300

    replaced hardcoded variables in beyonic payment

--- app/payments.py
+++ app/payments.py
@@ -4,15 +4,12 @@ from project.settings import BEYONIC_KEY
 beyonic.api_key = BEYONIC_KEY
 
 
-# def initialize_payment(phonenumber, first_name, last_name, amount, metadata: {}, ):
-def initialize_payment():
-    collection_request = beyonic.CollectionRequest.create(phonenumber='+256756878460',
-                                                          amount='500',
+def initialize_payment(phonenumber, description, metadata, amount=500):
+    collection_request = beyonic.CollectionRequest.create(phonenumber=phonenumber,
+                                                          amount=amount,
                                                           currency='UGX',
-                                                          description='Per diem',
+                                                          description=description,
                                                           callback_url='https://my.website/payments/callback',
-                                                          metadata={'my_id': '123ASDAsd123'},
-                                                          send_instructions=True
-                                                          )
-
+                                                          metadata=metadata,
+                                                          send_instructions=True)
     print(collection_request)
